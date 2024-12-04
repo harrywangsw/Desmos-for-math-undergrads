ca6482312d1223b85d8dcfd1fbe94fa55660e7cd
Your Name <your.email@example.com>
2024-12-03 20:49:16 -0500

    checkstyle

--- src/main/java/use_case/PreviousGraphsInteractor.java
+++ src/main/java/use_case/PreviousGraphsInteractor.java
@@ -1,12 +1,11 @@
 package use_case;
 
-import use_case.main.GraphDataAccessInterface;
-import use_case.note.DataAccessException;
-import use_case.previous_graphs.PreviousGraphsInputBoundary;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import use_case.note.DataAccessException;
+import use_case.previous_graphs.PreviousGraphsInputBoundary;
 
 public class PreviousGraphsInteractor implements PreviousGraphsInputBoundary {
     private final PreviousGraphsOutputBoundary previousGraphsOutputBoundary;
@@ -17,12 +16,12 @@ public class PreviousGraphsInteractor implements PreviousGraphsInputBoundary {
 
     @Override
     public void executePreviousGraphs() throws DataAccessException {
-        List<String> graphPaths = new ArrayList<>();
+        final List<String> graphPaths = new ArrayList<>();
 
-        File graphFolder = new File("./graphs/");
+        final File graphFolder = new File("./graphs/");
 
         if (graphFolder.exists() && graphFolder.isDirectory()) {
-            File[] files = graphFolder.listFiles();
+            final File[] files = graphFolder.listFiles();
             if (files != null) {
                 for (File file : files) {
                     if (file.isFile()) {
